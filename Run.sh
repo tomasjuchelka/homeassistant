a1dcf1f7d047a717123ce43934c42f3da2f88024
Tomas Juchelka <tomas.juchelka@gmail.com>
2020-11-07 15:16:33 +0100

    added new sensors and connection to influxdb

--- Run.sh
+++ Run.sh
@@ -6,3 +6,4 @@ docker rm home-assistant
 docker pull homeassistant/raspberrypi4-homeassistant:stable
 docker run -d --net=host --device=/dev/serial/by-id/usb-0658_0200-if00 --name home-assistant -v /home/pi/homeassistant:/config --restart=always homeassistant/raspberrypi4-homeassistant:stable
 docker start home-assistant
+docker image prune -f
