648135f83d151cc7cfb964b69bcb7c2043b71e7f
Tomas Juchelka <tomas.juchelka@gmail.com>
2020-11-08 13:59:48 +0100

    moved from zwave to ozw

--- Run.sh
+++ Run.sh
@@ -4,6 +4,6 @@ docker ps
 docker stop home-assistant
 docker rm home-assistant
 docker pull homeassistant/raspberrypi4-homeassistant:stable
-docker run -d --net=host --device=/dev/serial/by-id/usb-0658_0200-if00 --name home-assistant -v /home/pi/homeassistant:/config --restart=always homeassistant/raspberrypi4-homeassistant:stable
+docker run -d --net=host --name home-assistant -v /home/pi/docker/homeassistant:/config -v /home/pi/media:/media -v /etc/localtime:/etc/localtime:ro --restart=always homeassistant/raspberrypi4-homeassistant:stable
 docker start home-assistant
 docker image prune -f
